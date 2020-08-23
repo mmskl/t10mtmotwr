6352e650088ef101ef44c85bfeb66ace3e46e562
M.Moskal <contact@mmskl.net>
2020-08-23 03:09:58 +0200

    Uniq links

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ http.createServer(async (req, res) => {
                 item = {
                     title: title,
                     // TF doesn't keep links to all archive pages
-                    link: 'https://torrentfreak.com/top-10-most-torrented-pirated-movies/',
+                    link: 'https://torrentfreak.com/top-10-most-torrented-pirated-movies?hash=' + hash,
                     description: table,
                 }
 
